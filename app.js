7a0f347ad2f2eb247e6fe10d60b519f7c76f3bde
macmartinsRR <macmartins@runtime-revolution.com>
2021-11-30 10:47:50 +0000

    Mongo cs log

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const port = process.env.PORT || 3000;
 app.use("/assets", express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 
+console.log(config.getDbConnectionString())
 mongoose.connect(config.getDbConnectionString(), () =>
   console.log("connected")
 );
