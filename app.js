971115b5ed24de7277da6a4791f695d98e7347c5
macmartinsRR <macmartins@runtime-revolution.com>
2021-11-30 10:50:23 +0000

    Removed cs log

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ const port = process.env.PORT || 3000;
 app.use("/assets", express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 
-console.log(config.getDbConnectionString())
 mongoose.connect(config.getDbConnectionString(), () =>
   console.log("connected")
 );
